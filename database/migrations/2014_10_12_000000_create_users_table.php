00baf24c9c75e916103d0b247d9e9f9cfe341938
hendraaaa <hendraa3005@gmail.com>
2022-04-23 23:03:04 +0800

    slicing pages

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,8 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password')->nullable();
-            $table->string('avatar')->nulllable();
-            $table->string('occupation')->nulllable();
+            $table->string('avatar')->nullable();
+            $table->string('occupation')->nullable();
             $table->boolean('is_admin')->default(false);
             $table->rememberToken();
             $table->timestamps();
